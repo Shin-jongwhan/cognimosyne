768234af967e801e4e667e717307eb7e26827ac8
Shin-jongwhan <shinejh0528@gmail.com>
2025-05-21 13:37:18 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Hero from './components/Hero'
+import Process from './components/Process'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div>
+      <Hero />
+      <Process />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
