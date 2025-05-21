3639c0542d22b7c3c0a82e7a7ce33ec1f5f51c69
Shin-jongwhan <shinejh0528@gmail.com>
2025-05-21 13:57:08 +0900

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/cognimosyne/',  // ✅ 반드시 넣어야 GitHub Pages에서 작동
   plugins: [react()],
 })
