360484fbc7f9607e9b47afad98f49aa760b42782
Shin-jongwhan <shinejh0528@gmail.com>
2025-05-22 11:19:00 +0900

    .

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/cognimosyne/" element={<LandingPage />} />
+        <Route path="/" element={<LandingPage />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
