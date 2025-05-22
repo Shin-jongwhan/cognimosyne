efc295fb28436d8816944f49738639a2e7e63e8b
Shin-jongwhan <shinejh0528@gmail.com>
2025-05-23 00:37:17 +0900

    '.'

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,39 @@
-// /src/main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import PolicyPage from "./pages/PolicyPage";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <BrowserRouter>
+function Redirector() {
+  const navigate = useNavigate();
+  React.useEffect(() => {
+    const redirectPath = sessionStorage.getItem("redirect");
+    if (redirectPath) {
+      sessionStorage.removeItem("redirect");
+      navigate(redirectPath);
+    }
+  }, [navigate]);
+
+  return null;
+}
+
+function RouterApp() {
+  return (
+    <>
+      <Redirector />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/policy" element={<PolicyPage />} />
       </Routes>
+    </>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <RouterApp />
     </BrowserRouter>
   </React.StrictMode>
 );
